51bd695a68de1bd1a2f5d13188eca21508c3294b
Evan Moore <etmoore@gmail.com>
2014-07-27 19:40:49 -0600

    created test.php and added simple php copyright date

---
+++ test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Shirts 4 Mike</title>
+</head>
+<body>
+  <h1>Shirts 4 Mike</h1>
+  <p>&copy;<?php echo date ('Y'); ?> Shirts 4 Mike</p>
+</body>
+</html>
