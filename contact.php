a6fbb8b47189c935fb77541eae056cf4cc58fc1b
Evan Moore <etmoore@gmail.com>
2014-08-17 17:38:45 -0600

    added form for email

--- contact.php
+++ contact.php
@@ -1,9 +1,42 @@
 <?php
 $pageTitle = "Contact Mike";
+$section = "contact";
 include('inc/header.php'); ?>
 
   <div class="section page">
-    <h1>Contact</h1>
+    <div class="wrapper">
+      <h1>Contact</h1>
+      <p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>
+      <form action="" method="post">
+        <table>
+          <tr>
+            <th>
+              <label for="name">Name</label>
+            </th>
+            <td>
+              <input type="text" name="name" id="name">
+            </td>
+          </tr>
+          <tr>
+            <th>
+              <label for="email">Email</label>
+            </th>
+            <td>
+              <input type="text" name="email" id="email">
+            </td>
+          </tr>
+          <tr>
+            <th>
+              <label for="message">Message</label>
+            </th>
+            <td>
+              <textarea name="message" id="message"></textarea>
+            </td>
+          </tr>
+        </table>
+        <input type="submit" value="Send">
+      </form>
+    </div>
   </div>
 
 <?php include('inc/footer'); ?>
